e5c44aa50c9540695b222732259ac4e1c1356d0a
Shahebaj Inamdar <inamdar.shahebaj@gmail.com>
2019-07-10 20:12:15 +0530

    basic routing of app added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { HomeComponent } from './core/components/home/home.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+  , {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+  , {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
