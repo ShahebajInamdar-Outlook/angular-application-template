ff313bcdd9618fe00a170093fbc88008a2ea6c0f
Shahebaj Inamdar <inamdar.shahebaj@gmail.com>
2019-07-30 12:25:45 +0530

    code refactored and added some tasks to do.

--- src/app/core/models/http.model.ts
+++ src/app/core/models/http.model.ts
@@ -1,12 +1,23 @@
 import { HttpMethodTypeEnum } from '../enums/http-method-type.enum';
 
 export class HttpRequestModel {
+    constructor() {
+        this.data = null;
+    }
+
     url: string;
     httpMethodTypeId: HttpMethodTypeEnum;
     data: any;
 }
 
 export class HttpResponseModel {
+    constructor() {
+        this.isSuccess = false;
+        this.data = null;
+        this.isError = false;
+        this.error = null;
+    }
+
     isSuccess: boolean;
     data: any;
     isError: boolean;
